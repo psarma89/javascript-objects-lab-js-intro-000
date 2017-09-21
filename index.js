10d63c37d0439c335c8cb85aae243d5af7c0857e
Priyam Sarma <priyam.sarma@gmail.com>
2017-09-21 17:19:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function deleteFromObjectByKey(object, key){
   return newObject
 }
 
-function destructivelyDeleteFromObjectByKey(object,ket){
+function destructivelyDeleteFromObjectByKey(object,key){
   delete object[key]
   return object
 }
